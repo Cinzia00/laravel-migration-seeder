b7a574995451d3a275ba5de07d0c7459146ae67b
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-04-12 16:20:40 +0200

    Feat creata tabella trains

---
+++ database/migrations/2023_04_12_131616_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda',35);
+            $table->string('stazione_di_partenza',20);
+            $table->string('stazione_di_arrivo',20);
+            $table->dateTime('data_e_orario_di_partenza');
+            $table->dateTime('data_e_orario_di_arrivo');
+            $table->string('codice_treno',10);
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->tinyInteger('numero_totale_posti');
+            $table->tinyInteger('numero_totale_passeggeri');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
